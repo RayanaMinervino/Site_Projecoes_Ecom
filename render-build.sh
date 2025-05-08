eddf2e2a0e0302904b0567e87f239f26316a3b78
Rayana Minervino <minervinorayana@gmail.com>
2025-05-08 17:05:51 -0300

    Corrige driver para ODBC 17 compatível com o Render

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-# Atualiza o sistema e instala o ODBC Driver 18 para SQL Server
+# Instala o driver ODBC 18 for SQL Server no Render
 
-apt-get update && apt-get install -y curl gnupg
+apt-get update && apt-get install -y curl gnupg apt-transport-https
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
 apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql18
+ACCEPT_EULA=Y apt-get install -y msodbcsql18 unixodbc-dev
+
