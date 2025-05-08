0add57e2bb6ce29c9dc597af82914f5e61190b7d
RayanaMinervino <minervinorayana@gmail.com>
2025-05-08 15:34:58 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-# Atualiza o sistema e instala o driver do SQL Server
+# Atualiza o sistema e instala o driver do SQL Server 18 (necessário para conexão via pyodbc)
 
 apt-get update && apt-get install -y gnupg curl apt-transport-https
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
 apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql17
+ACCEPT_EULA=Y apt-get install -y msodbcsql18
+
