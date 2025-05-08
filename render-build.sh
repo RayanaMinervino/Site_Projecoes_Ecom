f45129998caf2cc4950544603e669944a6b592dd
RayanaMinervino <minervinorayana@gmail.com>
2025-05-08 15:09:53 -0300

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Atualiza o sistema e instala o driver do SQL Server
+
+apt-get update && apt-get install -y gnupg curl apt-transport-https
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql17
